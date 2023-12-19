f398adf65fee76891874b38fbacf443c66518dbd
ノーパソよりま <yoribaseball623@gmail.com>
2023-12-19 15:12:55 +0900

    し忘れ

--- Mythpract a/Assets/Yori/Script/TutorialText.cs
+++ Mythpract a/Assets/Yori/Script/TutorialText.cs
@@ -5,8 +5,23 @@ using UnityEngine.InputSystem;
 
 public class TutorialText : MonoBehaviour
 {
-    [SerializeField]
-    
+    private enum PopTextType
+    {
+        Move=0,
+        Jump=1,
+        WJump=2,
+        NAttack=3,
+        SAttack=4,
+        Skill=5,
+        Guard=6,
+        Blink=7
+    }
+    [SerializeField, Header("検知範囲")]
+    private PopTextType popText = PopTextType.Move;
+    private void Awake()
+    {
+        
+    }
     void Start()
     {
         this.GetComponent<TextMesh>().text = GameData.lefttx + " " + GameData.righttx + "で移動";
