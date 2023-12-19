72eb5aef0adee00f34ad454eff904947aae5b5ec
ノーパソよりま <yoribaseball623@gmail.com>
2023-12-19 14:07:06 +0900

    サンプルタイトル作成 Tutorial途中

--- Mythpract a/Assets/Yori/Script/TutorialText.cs
+++ Mythpract a/Assets/Yori/Script/TutorialText.cs
@@ -5,10 +5,11 @@ using UnityEngine.InputSystem;
 
 public class TutorialText : MonoBehaviour
 {
-
+    [SerializeField]
+    
     void Start()
     {
-        
+        this.GetComponent<TextMesh>().text = GameData.lefttx + " " + GameData.righttx + "で移動";
     }
     void Update()
     {
