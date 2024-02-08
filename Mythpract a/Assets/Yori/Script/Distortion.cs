f43770cffaed2bca9dc3eafaccb1effb6cf512d2
ノーパソよりま <yoribaseball623@gmail.com>
2024-02-08 15:13:09 +0900

    タイトル微調整中

--- Mythpract a/Assets/Yori/Script/Distortion.cs
+++ Mythpract a/Assets/Yori/Script/Distortion.cs
@@ -1,22 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Distortion : MonoBehaviour
 {
     public Material dismat;
     private float disspd = 1f;
-    [SerializeField,Header("消える速度")]
+    [SerializeField, Header("出現速度")]
     private float fadeSpd = 0.01f;
+
+    [SerializeField, Header("タイトルキャンバス")]
+    private Canvas titleCanvas;
 
     // Update is called once per frame
     void Update()
     {
-        if (disspd >= 0)
+        // デバッグ用プログラム
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            disspd = 1;
+        }
+        
+        if (disspd <= 1)
         {
             disspd -= fadeSpd;
         }
-        
+        else
+        {
+            
+        }
         dismat.SetFloat("DissolveAmount", disspd);
     }
 }
