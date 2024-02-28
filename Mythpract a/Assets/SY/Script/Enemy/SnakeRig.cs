240eab3e1265c4eafbab244f86430aad30736d8b
SY <nkc20226177@st.denpa.jp>
2024-02-28 11:35:10 +0900

    ショゴス：Rig更新

--- Mythpract a/Assets/SY/Script/Enemy/SnakeRig.cs
+++ Mythpract a/Assets/SY/Script/Enemy/SnakeRig.cs
@@ -6,56 +6,106 @@ namespace SY
 {
     public class SnakeRig : MonoBehaviour
     {
-        [SerializeField, Tooltip("親")] GameObject root;
-        [SerializeField, Tooltip("子")] GameObject[] body;
-        [SerializeField, Tooltip("隙間")] float space = 1.0f;
+        [Header("オブジェクト")]
+        [SerializeField, Tooltip("親")] GameObject rootBone;
+        [SerializeField, Tooltip("子")] GameObject[] childBone;
+        [SerializeField, Tooltip("スプライト")] GameObject[] model;
 
-        Vector2 rootPos;    //親位置
-        Vector2[] bodyPos;  //子位置
-        Quaternion rot;     //回転値
-        Vector3 angle;      //角度(deg)
-        Vector3 defScale;   //初期拡大率
-        Vector3 scale;      //拡大率(上下反転)
+        [Header("隙間")]
+        [SerializeField, Tooltip("親子")] float rootSpace = 1.0f; //ver3.0で追加
+        [SerializeField, Tooltip("子")] float BodySpace = 1.0f;
+
+        [Header("描画設定")]
+        [SerializeField, Tooltip("子描画順")] Sort modelSpriteOrder = Sort.None;
+
+        enum Sort
+        {
+            None,
+            Zero,
+            BodyLength,
+            Ascending,
+            Descending,
+        }
+
+        Vector2 rootPos;        //親位置
+        Vector2 beforeRootPos;  //親位置保存
+        Vector2[] childPos;     //子位置
+        Quaternion rot;         //回転値
+        Vector3 angle;          //角度(deg)
+        Vector3 defScale;       //初期拡大率
+        Vector3 scale;          //拡大率(上下反転)
 
         Vector2 del;    //移動量
 
-        public GameObject Root { get { return root; } }
-        public GameObject[] Body { get { return body; } }
-        public float Space { get { return space; } }
+        public GameObject Root { get { return rootBone; } }
+        public GameObject[] Body { get { return childBone; } }
+        public GameObject[] Model { get { return model; } }
 
         // Start is called before the first frame update
         void Start()
         {
-            bodyPos = new Vector2[body.Length];     //配列要素数指定
-            defScale = root.transform.lossyScale;   //拡大率保存
+            childPos = new Vector2[childBone.Length];   //配列要素数定義
+            defScale = model[0].transform.lossyScale;   //大きさ保存
 
-            //各オブジェクト座標保存
-            rootPos = root.transform.position;
-            for (int i = 0; i < body.Length; i++)
+            rootPos = rootBone.transform.position;  //親位置保存
+            beforeRootPos = rootPos;
+            for (int i = 0; i < childBone.Length; i++)
             {
-                bodyPos[i] = body[i].transform.position;
+                childPos[i] = childBone[i].transform.position;  //子位置保存
+                SpriteOrder(model[i], modelSpriteOrder, i);     //モデル描画順設定
             }
         }
 
         // Update is called once per frame
         void Update()
         {
-            rootPos = root.transform.position;
+            rootPos = rootBone.transform.position;
 
-            for (int i = 0; i < body.Length; i++)
+            del = Delta(childPos[0], rootPos);  //ボーン間距離
+            Move(del, rootSpace, 0);            //移動
+
+            for (int i = 1; i < childBone.Length; i++)
             {
-                //初回のみ親の座標を参照
-                if (0 <= i - 1) { del = Delta(bodyPos[i], bodyPos[i - 1]); }
-                else { del = Delta(bodyPos[i], rootPos); }
+                del = Delta(childPos[i], childPos[i - 1]);
 
-                //一定以上の隙間があれば処理
-                if (Magnitude(del) <= space) { break; }
-
-                bodyPos[i] += del - (Normalize(del) * space);   //移動
-                body[i].transform.position = bodyPos[i];        //移動適用
-
-                Rotate(body[i], del);   //回転
+                Move(del, BodySpace, i);
             }
+
+            beforeRootPos = rootPos;
+        }
+
+        /// <summary>
+        /// 移動（ver3.0で追加）
+        /// </summary>
+        /// <param name="delta">移動量</param>
+        /// <param name="space">隙間</param>
+        /// <param name="no">配列要素数</param>
+        void Move(Vector2 delta, float space, int no)
+        {
+            if (rootPos == beforeRootPos) { return; }
+
+            //一定以上の隙間があれば処理
+            if (space < Magnitude(delta))
+            {
+                childPos[no] += delta - (Normalize(delta) * (space - 0.01f));  //移動
+                childBone[no].transform.position = childPos[no];  //移動適用
+            }
+
+            if (no == 0)    //初回のみ親参照
+            {
+                //2点の中心座標
+                model[no].transform.position = new Vector2(
+                    (rootPos.x + childPos[no].x) * 0.5f,
+                    (rootPos.y + childPos[no].y) * 0.5f);
+            }
+            else            //初回以外は子同士
+            {
+                model[no].transform.position = new Vector2(
+                    (childPos[no - 1].x + childPos[no].x) * 0.5f,
+                    (childPos[no - 1].y + childPos[no].y) * 0.5f);
+            }
+
+            Rotate(model[no], del); //回転
         }
 
         /// <summary>
@@ -65,8 +115,6 @@ namespace SY
         /// <param name="vec">移動方向</param>
         public void Rotate(GameObject obj, Vector2 vec)
         {
-            if (vec == Vector2.zero) { return; }    //動いていなければ現状維持
-
             //回転
             rot = Quaternion.FromToRotation(Vector3.up, vec);   //移動方向の回転値を取得
             angle = rot.eulerAngles;        //クォータニオンをディグリーに変換
@@ -106,5 +154,25 @@ namespace SY
         /// <param name="delta">移動量</param>
         /// <returns></returns>
         Vector2 Normalize(Vector2 delta) { return delta.normalized; }
+
+        //----------描画順
+        void SpriteOrder(GameObject obj, Sort sort, int value)
+        {
+            switch (sort)
+            {
+                case Sort.Zero:
+                    obj.GetComponent<SpriteRenderer>().sortingOrder = 0;
+                    break;
+                case Sort.BodyLength:
+                    obj.GetComponent<SpriteRenderer>().sortingOrder = model.Length;
+                    break;
+                case Sort.Ascending:
+                    obj.GetComponent<SpriteRenderer>().sortingOrder = value;
+                    break;
+                case Sort.Descending:
+                    obj.GetComponent<SpriteRenderer>().sortingOrder = model.Length - 1 - value;
+                    break;
+            }
+        }
     }
 }
