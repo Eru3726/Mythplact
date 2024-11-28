6d919600140fc9921adca27810f4d09d3aa9eb8e
maato <hurutamanato@gmail.com>
2024-11-28 11:17:56 +0900

    スクリプト追加

--- Mythpract a/Assets/Script/Player/State/Instance/PlayerBlinkState.cs
+++ Mythpract a/Assets/Script/Player/State/Instance/PlayerBlinkState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IInput : MonoBehaviour
+public class PlayerBlinkState : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
